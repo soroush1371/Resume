fa94d0e3d459dff31fb40cd2f07c55ec4882ce60
Soroush Shahnazian <S.Shahnazian@AFGC.local>
2019-12-09 15:23:23 +0330

    Updates

---
+++ precache-manifest.232cca55357fdee752be0c5f8f17af31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eac49a9562a0a59d25effb9e49a6a85",
+    "url": "/MyResume/index.html"
+  },
+  {
+    "revision": "fb77eacfd27fab019ca0",
+    "url": "/MyResume/static/css/main.0f4bcb9b.chunk.css"
+  },
+  {
+    "revision": "c48aaa2d8c8cf8ed0f15",
+    "url": "/MyResume/static/js/2.c0999a2d.chunk.js"
+  },
+  {
+    "revision": "fb77eacfd27fab019ca0",
+    "url": "/MyResume/static/js/main.3d59db92.chunk.js"
+  },
+  {
+    "revision": "fc5cd5cf6293c02e310d",
+    "url": "/MyResume/static/js/runtime-main.74aa8dc4.js"
+  }
+]);
